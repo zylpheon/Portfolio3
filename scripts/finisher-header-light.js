0b51f424f83bcb871dc43cfae1085fabf54077d5
zylpheon <lrv94451@gmail.com>
2025-08-11 20:24:00 +0700

    hElL0 WoRld

---
+++ scripts/finisher-header-light.js
@@ -0,0 +1,35 @@
+new FinisherHeader({
+    "count": 10,
+    "size": {
+        "min": 1300,
+        "max": 1500,
+        "pulse": 0
+    },
+    "speed": {
+        "x": {
+            "min": 0.4,
+            "max": 0.8
+        },
+        "y": {
+            "min": 0.1,
+            "max": 0.6
+        }
+    },
+    "colors": {
+        "background": "#8C8C8C",
+        "particles": [
+            "#A8A8A8",
+            "#C4C4C4",
+            "#E0E0E0"
+        ]
+    },
+    "blending": "overlay",
+    "opacity": {
+        "center": 0.5,
+        "edge": 0.05
+    },
+    "skew": 0,
+    "shapes": [
+        "c"
+    ]
+});
