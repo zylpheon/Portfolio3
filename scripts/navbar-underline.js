505d133d8bd3992847fcf84ff930e69ae89d2595
zylpheon <lrv94451@gmail.com>
2025-09-06 07:32:57 +0700

    c3Rt1fIc4tEs F1xiN9

--- scripts/navbar-underline.js
+++ scripts/navbar-underline.js
@@ -4,7 +4,6 @@ window.addEventListener('DOMContentLoaded', function () {
     const links = nav.querySelectorAll('.nav-link');
     const underline = nav.querySelector('.nav-underline');
     let activeIndex = 0;
-
     function moveUnderlineTo(el) {
         let left = el.offsetLeft;
         const style = window.getComputedStyle(el);
@@ -19,9 +18,7 @@ window.addEventListener('DOMContentLoaded', function () {
         }
         underline.style.background = color;
     }
-
     moveUnderlineTo(links[activeIndex]);
-
     links.forEach((link, idx) => {
         link.addEventListener('mouseenter', e => {
             moveUnderlineTo(link);
@@ -30,47 +27,36 @@ window.addEventListener('DOMContentLoaded', function () {
             moveUnderlineTo(links[activeIndex]);
         });
     });
-
     nav.addEventListener('mouseleave', () => {
         moveUnderlineTo(links[activeIndex]);
     });
-
     window.addEventListener('resize', () => moveUnderlineTo(links[activeIndex]));
-
     const observer = new MutationObserver(() => moveUnderlineTo(links[activeIndex]));
     observer.observe(document.body, { attributes: true, attributeFilter: ['class'] });
-
-    // Page navigation events
     document.addEventListener('homePageActive', function () {
         activeIndex = 0;
         moveUnderlineTo(links[activeIndex]);
     });
-
     document.addEventListener('aboutPageActive', function () {
         activeIndex = 1;
         moveUnderlineTo(links[activeIndex]);
     });
-
     document.addEventListener('skillsPageActive', function () {
         activeIndex = 2;
         moveUnderlineTo(links[activeIndex]);
     });
-
     document.addEventListener('certificatesPageActive', function () {
         activeIndex = 3;
         moveUnderlineTo(links[activeIndex]);
     });
-
     document.addEventListener('projectsPageActive', function () {
         activeIndex = 4;
         moveUnderlineTo(links[activeIndex]);
     });
-
     document.addEventListener('blogPageActive', function () {
         activeIndex = 5;
         moveUnderlineTo(links[activeIndex]);
     });
-
     document.addEventListener('contactPageActive', function () {
         activeIndex = 6;
         moveUnderlineTo(links[activeIndex]);
